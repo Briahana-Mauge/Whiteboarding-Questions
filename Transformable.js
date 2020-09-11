e8a198c2e786e2aed713a71000258bd4dbad91fc
Bpmauge <briahanamauge@pursuit.org>
2020-09-11 10:26:53 -0400

    refactor function

--- Transformable.js
+++ Transformable.js
@@ -13,30 +13,30 @@ What if str1 is an empty string? Start with the case where str1 === str2 and the
  */
 
 const newString = (str1, str2) => {
-    let str2Obj = {};
-  
-    if(str2.length < str1.length || !str1.length) return false;
-    for(let i = 0; i< str2.length; i++){
-      if(!str2Obj[str2[i]]){
-        str2Obj[str2[i]] = 1;
-      }else{
-        str2Obj[str2[i]]++;
-      }
-    }
-    for(let i = 0; i<str1.length; i++){
-      if(str2Obj[str1[i]]) {
-        str2Obj[str1[i]]--;
-      } else {
-        return false;
-        }
-    }
-    return true;
-  }
+  let str2Obj = {};
 
-  console.log(newString('aba','abcd')) //false
-  console.log(newString('aba', 'aabbaa')) //true
-  console.log(newString('cat', 'tacocta')) //true
-  console.log(newString('wow', 'dinner')) //false
-  console.log(newString('wow', 'dinner')) //false
-  console.log(newString('wow', 'wo')) //false
-  console.log(newString('', 'test')) //false
+  if (str2.length < str1.length || !str1.length) return false;
+  for (let i = 0; i < str2.length; i++) {
+    if (!str2Obj[str2[i]]) {
+      str2Obj[str2[i]] = 1;
+    } else {
+      str2Obj[str2[i]]++;
+    }
+  }
+  for (let i = 0; i < str1.length; i++) {
+    if (str2Obj[str1[i]]) {
+      str2Obj[str1[i]]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(newString('aba', 'abcd')) //false
+console.log(newString('aba', 'aabbaa')) //true
+console.log(newString('cat', 'tacocta')) //true
+console.log(newString('wow', 'dinner')) //false
+console.log(newString('women', 'woman')) //false
+console.log(newString('wow', 'wo')) //false
+console.log(newString('', 'test')) //false
